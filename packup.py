087c529e790c16540a1699108eecf65521648835
Ansh Kumar Triapthi <anshtrips07@gmail.com>
2024-09-17 20:08:41 +0530

    Main code for the backup code

---
+++ packup.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+import datetime
+import schedule
+import time
+
+source_dir = "Enter your source folder path"
+destination_dir = "Enter your destination folder path"
+
+def copy_folder_to_directory(source,dest):
+  today = datetime.date.today()
+  dest_dir = os.path.join(dest,str(today))
+
+  try:
+    shutil.copytree(source,dest_dir)
+    print(f"Folder copy to: {dest_dir}")
+  except FileExistsError:
+    print(f"Folder already exists in: {dest}")
+
+schedule.every().day.at("18:55").do(lambda: copy_folder_to_directory(source_dir,destination_dir))
+
+while True:
+  schedule.run_pending()
+  time.sleep(60)
